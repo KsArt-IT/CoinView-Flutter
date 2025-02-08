d585236bd0e8e956d317f64d874c389d3b1f9e08
KsArT-IT <ksart.it@gmail.com>
2025-02-08 20:54:44 +0200

    feat: create screens.dart to export CoinListScreen and CoinDetailScreen

---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,5 @@
+import 'coin_list/coins_list_screen.dart';
+import 'coin_detail/coin_detail_screen.dart';
+
+export 'coin_list/coins_list_screen.dart';
+export 'coin_detail/coin_detail_screen.dart';

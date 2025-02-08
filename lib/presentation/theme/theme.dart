c107ef76bc9c999ed21c4a6881d5c5389d403ee8
KsArT-IT <ksart.it@gmail.com>
2025-02-08 20:54:09 +0200

    feat: add light and dark theme configurations

---
+++ lib/presentation/theme/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  useMaterial3: true,
+);
+
+final darkTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  useMaterial3: true,
+);

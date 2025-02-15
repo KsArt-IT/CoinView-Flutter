03257c7a5c5465877eb9cfdcddcc48bf6afee7c3
KsArT-IT <ksart.it@gmail.com>
2025-02-15 17:00:42 +0200

    feat: implement dependency injection with Provider

--- app/lib/app/main.dart
+++ app/lib/app/main.dart
@@ -1,6 +1,12 @@
 import 'package:coin_view/app/coin_view_app.dart';
+import 'package:coin_view/di/di_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const CoinViewApp());
+  runApp(
+    MultiProvider(
+        providers: DIProvider.getProviders(),
+        child: const CoinViewApp()),
+  );
 }
---
+++ app/lib/di/di_provider.dart
@@ -0,0 +1,25 @@
+import 'package:coin_view/data/repositories/coin_repository_impl.dart';
+import 'package:coin_view/data/service/coin_service.dart';
+import 'package:coin_view/data/service/coin_service_impl.dart';
+import 'package:coin_view/domain/repositories/coin_repository.dart';
+import 'package:coin_view/ui/screens/viewmodels.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+interface class DIProvider {
+  static List<SingleChildWidget> getProviders() {
+    return [
+      Provider<CoinService>(create: (_) => CoinServiceImpl()),
+      ProxyProvider<CoinService, CoinRepository>(
+        create: (_) => CoinRepositoryImpl(service: null),
+        update: (_, service, previous) =>
+            CoinRepositoryImpl(service: service),
+      ),
+      ChangeNotifierProxyProvider<CoinRepository, CoinsViewModel>(
+        create: (_) => CoinsViewModel(repository: null), // Временное создание
+        update: (_, repository, previous) =>
+            CoinsViewModel(repository: repository),
+      ),
+    ];
+  }
+}

3b280f6e27987ac3f5abbf2fdee29a2b9c11abf2
KsArT-IT <ksart.it@gmail.com>
2025-02-20 20:42:09 +0200

    feat: implement CoinDetailScreen with ViewModel integration and loading state

--- app/lib/ui/screens/coin_detail/coin_detail_screen.dart
+++ app/lib/ui/screens/coin_detail/coin_detail_screen.dart
@@ -1,9 +1,11 @@
 import 'package:coin_view/router/router.dart';
+import 'package:coin_view/ui/screens/coin_detail/coin_detail_viewmodel.dart';
+import 'package:coin_view/ui/widgets/snackbar_ext.dart';
 import 'package:flutter/material.dart';
 
 class CoinDetailScreen extends StatefulWidget {
-
-  const CoinDetailScreen({super.key});
+  const CoinDetailScreen({super.key, required this.viewModel});
+  final CoinDetailViewModel viewModel;
 
   @override
   State<CoinDetailScreen> createState() => _CoinDetailScreenState();
@@ -18,23 +20,32 @@ class _CoinDetailScreenState extends State<CoinDetailScreen> {
     assert(args is String, "ID должен быть целым числом");
     _id = args as String;
     super.didChangeDependencies();
+    if (_id.isNotEmpty) {
+      widget.viewModel.fetchCoinDetail(_id);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Детали #$_id')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('ID: $_id', style: const TextStyle(fontSize: 24)),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => AppRoute.goBack(context),
-              child: const Text('Назад'),
-            ),
-          ],
+      body: SafeArea(
+        child: ListenableBuilder(
+          listenable: widget.viewModel,
+          builder: (context, _) {
+            if (widget.viewModel.message.isNotEmpty) {
+               Future.microtask(() {
+                context.showSnackbar(widget.viewModel.message);
+                widget.viewModel.clearMessage();
+              });
+            }
+
+            final coinDetail = widget.viewModel.coinDetail;
+            if (coinDetail != null) {
+              return Center(child: Text(coinDetail.name));
+            }
+            return const Center(child: CircularProgressIndicator());
+          },
         ),
       ),
     );
